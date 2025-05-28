bb2d161b71f80b2a90ec709112d4e5bb45055ec3
kanweitech <kanweiedward@gmail.com>
2025-05-28 10:04:54 -0700

    first commit

---
+++ store/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+    
+
+def aboutPage(request):
+    return render(request, 'about.html')
+
+# Create your views here.
